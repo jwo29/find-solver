fd7bc10e171fb4e588cb4545ca38f486de50219d
jiwoo <jwo2955@gmail.com>
2022-08-08 20:07:54 +0900

    vuejs 프로젝트 생성

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 메인 화면 윗부분(변하지 않음) -->
+    <div> 
+      <h1>is solved..?</h1>
+      <div>
+        <input type="text" placeholder="문제번호">
+        <button>검색</button>
+      </div>
+    </div>
+
+    <!-- 검색 결과 부분 -->
+    <div>
+      테이블 위
+      <user-table></user-table>
+      테이블 아래
+    </div>
+  </div>
+</template>
+
+<script>
+import UserTable from './UserTable.vue'
+
+export default {
+  components: { UserTable },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/components/UserTable.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- 표 그리기 -->
+    <div>
+        <table>
+            <th>BOJ ID</th>
+            <tr>
+                <td>one</td>
+            </tr>
+            <tr>
+                <td>one</td>
+            </tr>
+            <tr>
+                <td>one</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserTable',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style>
+
+</style>
