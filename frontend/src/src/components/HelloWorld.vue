330def9f647885174b5cbe47913b048e267676a6
Lee-Jiwoo <55731054+jwo29@users.noreply.github.com>
2022-08-12 11:24:22 +0900

    검색 및 유저 테이블 생성
    - 푼 문제로 푼 사람 검색 구현
    - 푼 사람 테이블 생성

---
+++ frontend/src/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- 메인 화면 윗부분(변하지 않음) -->
+    <div> 
+      <h1>is solved..?</h1>
+      <div>
+        <input id="search-input" v-model="pno" type="text" placeholder="문제번호">
+        <p>현재 입력 문제번호: {{ pno }}</p>
+      </div>
+    </div>
+
+    <!-- 검색 결과 부분 -->
+    <div>
+      테이블 위
+      <user-table :pno="pno"></user-table>
+      테이블 아래
+    </div>
+  </div>
+</template>
+
+<script>
+import UserTable from './UserTable.vue'
+
+export default {
+  components: { UserTable },
+  name: 'HelloWorld',
+  el: "#search-input",
+  data() {
+    return {
+      pno: "",
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
