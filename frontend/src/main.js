13c156133f37a87bc4346020227241823fd89799
jiwoo <jwo2955@gmail.com>
2022-12-19 22:17:53 +0900

    [수정] UI 구현

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
-
-createApp(App).mount('#app')
